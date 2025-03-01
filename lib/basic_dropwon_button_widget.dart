3db3c9f2594c05a970c0a25875bdd942e87310d1
DemianKryk <demian.ilnytskyi@codinghouse.biz>
2025-03-01 12:56:34 +0200

    tests

---
+++ lib/basic_dropwon_button_widget.dart
@@ -0,0 +1,382 @@
+import 'package:flutter/material.dart';
+
+/// A basic implementation of a drop-down button that handles internal state
+/// and displays the overlay menu.
+class BasicDropDownButton extends StatefulWidget {
+  /// Creates a [BasicDropDownButton].
+  ///
+  /// Either [buttonText] or [buttonChild] must be provided, and either
+  /// [menuItems]
+  /// or [menuList] must be provided.
+  const BasicDropDownButton({
+    required this.buttonStyle,
+    required this.buttonText,
+    required this.menuItems,
+    required this.menuVerticalSpacing,
+    required this.menuBackgroundColor,
+    required this.showIndicatorIcon,
+    required this.position,
+    required this.buttonCloseMenuIcon,
+    required this.buttonOpenMenuIcon,
+    Key? key,
+    this.itemSpacing = 8,
+    this.buttonIconColor = Colors.black,
+    this.buttonIconFirst = true,
+    this.menuItemsSpacing = 0,
+    this.menuClipBehavior = Clip.hardEdge,
+    this.menuItemsPadding,
+    this.buttonChild,
+    this.menuBorderRadius,
+    this.buttonTextStyle,
+    this.customButton,
+    this.menuList,
+    this.menuKey,
+  })  : assert(
+          !(buttonChild == null && buttonText == null && customButton == null),
+          'Either provide a [buttonText] or a custom [buttonChild].',
+        ),
+        assert(
+          !(menuItems == null && menuList == null),
+          'Either provide [menuItems] or a custom [menuList].',
+        ),
+        super(key: key);
+
+  /// Custom widget for the button. If provided, overrides [buttonText].
+  final Widget? buttonChild;
+
+  /// Builder function to generate the list of menu item widgets.
+  final List<Widget> Function(VoidCallback hideMenu)? menuItems;
+
+  /// Border radius for the drop-down menu.
+  final BorderRadius? menuBorderRadius;
+
+  /// Position of the drop-down menu relative to the button.
+  final DropDownButtonPosition position;
+
+  /// Button style for the main button.
+  final ButtonStyle buttonStyle;
+
+  /// Text displayed on the main button.
+  final String? buttonText;
+
+  /// Text style for the main button.
+  final TextStyle? buttonTextStyle;
+
+  /// Vertical spacing between the main button and the drop-down menu.
+  final double menuVerticalSpacing;
+
+  /// Icon to display when the menu is open.
+  final Widget? buttonCloseMenuIcon;
+
+  /// Icon to display when the menu is closed.
+  final Widget? buttonOpenMenuIcon;
+
+  /// Determines whether to show the indicator icon on the main button.
+  final bool showIndicatorIcon;
+
+  /// Padding for the menu items inside the drop-down.
+  final EdgeInsets? menuItemsPadding;
+
+  /// Background color of the drop-down menu.
+  final Color menuBackgroundColor;
+
+  /// Custom builder for the main button.
+  final Widget Function(VoidCallback? showHideMenu)? customButton;
+
+  /// Spacing between individual menu items in the drop-down.
+  final double itemSpacing;
+
+  /// Custom widget for the entire menu list. If provided, overrides
+  /// [menuItems].
+  final Widget? menuList;
+
+  /// Color for the icon displayed on the main button.
+  final Color buttonIconColor;
+
+  /// Spacing between menu items.
+  final double menuItemsSpacing;
+
+  /// Clipping behavior for the drop-down menu.
+  final Clip menuClipBehavior;
+
+  final Key? menuKey;
+
+  /// Alignment for the icon within the main button.
+  final bool buttonIconFirst;
+
+  @override
+  State<BasicDropDownButton> createState() => _BasicDropDownButtonState();
+}
+
+class _BasicDropDownButtonState extends State<BasicDropDownButton> {
+  late bool _showMenu;
+  late OverlayPortalController _controller;
+  late LayerLink _optionsLayerLink;
+  late double? menuHeight;
+  late GlobalKey _menuKey;
+  late GlobalKey _anchorKey;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = OverlayPortalController();
+    _optionsLayerLink = LayerLink();
+
+    _showMenu = false;
+    menuHeight = null;
+
+    _menuKey = GlobalKey(debugLabel: 'menu_key');
+    _anchorKey = GlobalKey(debugLabel: 'button_key');
+  }
+
+  /// Toggles the visibility of the drop-down menu.
+  void showHideMenu() {
+    _controller.toggle();
+    setState(() {
+      _showMenu = !_showMenu;
+    });
+  }
+
+  /// Sets the height of the drop-down menu after it has been rendered.
+  void setMenuHeight() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final context = _menuKey.currentContext;
+      if (context != null) {
+        final box = context.findRenderObject()! as RenderBox;
+        if (box.hasSize) {
+          setState(() {
+            menuHeight = box.size.height;
+          });
+        }
+      }
+    });
+  }
+
+  /// Returns the height of the anchor button.
+  double get getHeight {
+    final context = _anchorKey.currentContext;
+    if (context != null) {
+      final box = context.findRenderObject()! as RenderBox;
+      return box.hasSize ? box.size.height : 0;
+    }
+    return 0;
+  }
+
+  /// Determines the position of the drop-down menu based on available
+  /// screen space.
+  DropDownButtonPosition get _positionCalculate {
+    final renderObject = context.findRenderObject();
+
+    bool? hasBottomSpace;
+    bool? hasTopSpace;
+
+    if (menuHeight != null && renderObject != null) {
+      final renderBox = renderObject as RenderBox;
+      final screenHeight = MediaQuery.sizeOf(context).height;
+      final buttonPosition = renderBox.localToGlobal(Offset.zero);
+      final availableHeight = screenHeight - (buttonPosition.dy + getHeight);
+      hasBottomSpace = availableHeight > menuHeight!;
+      hasTopSpace = buttonPosition.dy > menuHeight!;
+    }
+
+    return widget.position.positionCalculate(
+      hasBottomPlace: hasBottomSpace,
+      hasTopPlace: hasTopSpace,
+    );
+  }
+
+  /// Builds the drop-down menu widget.
+  Widget _getMenuWidget(BuildContext context) {
+    final positionValue = _positionCalculate;
+    if (menuHeight == null) setMenuHeight();
+    final items = widget.menuItems?.call(showHideMenu);
+    final menu = CompositedTransformFollower(
+      link: _optionsLayerLink,
+      showWhenUnlinked: false,
+      targetAnchor: positionValue.getMenuPosition,
+      followerAnchor: positionValue.getContentPosition,
+      child: Align(
+        alignment: positionValue.getContentPosition,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: widget.menuVerticalSpacing,
+            bottom: widget.menuVerticalSpacing,
+          ),
+          child: TapRegion(
+            key: widget.menuKey,
+            onTapOutside: (event) => showHideMenu(),
+            child: widget.menuList ??
+                Container(
+                  key: _menuKey,
+                  decoration: BoxDecoration(
+                    color: widget.menuBackgroundColor,
+                    borderRadius: widget.menuBorderRadius,
+                  ),
+                  clipBehavior: widget.menuClipBehavior,
+                  child: Padding(
+                    padding: widget.menuItemsPadding ?? EdgeInsets.zero,
+                    child: IntrinsicWidth(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: List.generate(
+                          items!.length * 2,
+                          (index) {
+                            if (index.isOdd) {
+                              return SizedBox(
+                                height: widget.menuItemsSpacing,
+                              );
+                            } else {
+                              return items.elementAt((index / 2).toInt());
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+          ),
+        ),
+      ),
+    );
+    if (menuHeight == null) {
+      return Opacity(opacity: 0, child: menu);
+    } else {
+      return menu;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RepaintBoundary(
+      child: OverlayPortal.targetsRootOverlay(
+        controller: _controller,
+        overlayChildBuilder: _getMenuWidget,
+        child: CompositedTransformTarget(
+          link: _optionsLayerLink,
+          child: widget.customButton?.call(_showMenu ? null : showHideMenu) ??
+              _defaultButton,
+        ),
+      ),
+    );
+  }
+
+  /// Default button widget if no custom button is provided.
+  Widget get _defaultButton {
+    final icon = widget.showIndicatorIcon
+        ? _showMenu
+            ? widget.buttonCloseMenuIcon
+            : widget.buttonOpenMenuIcon
+        : null;
+    final textWidget = widget.buttonChild ??
+        Text(
+          widget.buttonText!,
+          style: widget.buttonTextStyle ?? const TextStyle(color: Colors.black),
+        );
+    final Widget child;
+    if (icon == null) {
+      child = textWidget;
+    } else {
+      child = Row(
+        mainAxisSize: MainAxisSize.min,
+        children: widget.buttonIconFirst
+            ? [
+                icon,
+                Flexible(child: textWidget),
+              ]
+            : [
+                Flexible(child: textWidget),
+                icon,
+              ],
+      );
+    }
+    return TextButton(
+      key: _anchorKey,
+      onPressed: _showMenu ? null : showHideMenu,
+      style: _showMenu
+          ? widget.buttonStyle.merge(
+              TextButton.styleFrom(
+                disabledMouseCursor: SystemMouseCursors.click,
+                iconColor: widget.buttonIconColor,
+              ),
+            )
+          : widget.buttonStyle,
+      child: child,
+    );
+  }
+}
+
+/// Enum representing the possible positions of the drop-down menu relative
+/// to the button.
+enum DropDownButtonPosition {
+  bottomCenter,
+  bottomRight,
+  bottomLeft,
+  topCenter,
+  topRight,
+  topLeft,
+}
+
+/// Extension on [DropDownButtonPosition] to calculate effective positioning
+/// and alignment.
+extension _DropDownButtonPositionExtension on DropDownButtonPosition {
+  /// Determines the effective position of the drop-down menu based on
+  /// available space.
+  DropDownButtonPosition positionCalculate({
+    required bool? hasBottomPlace,
+    required bool? hasTopPlace,
+  }) {
+    if (hasBottomPlace == null || hasTopPlace == null) return this;
+    switch (this) {
+      case DropDownButtonPosition.bottomCenter:
+        return hasBottomPlace ? this : DropDownButtonPosition.topCenter;
+      case DropDownButtonPosition.bottomLeft:
+        return hasBottomPlace ? this : DropDownButtonPosition.topLeft;
+      case DropDownButtonPosition.bottomRight:
+        return hasBottomPlace ? this : DropDownButtonPosition.topRight;
+      case DropDownButtonPosition.topCenter:
+        return hasTopPlace ? this : DropDownButtonPosition.bottomCenter;
+      case DropDownButtonPosition.topLeft:
+        return hasTopPlace ? this : DropDownButtonPosition.bottomLeft;
+      case DropDownButtonPosition.topRight:
+        return hasTopPlace ? this : DropDownButtonPosition.bottomRight;
+    }
+  }
+
+  /// Returns the alignment for the drop-down content based on the
+  /// current position.
+  Alignment get getContentPosition {
+    switch (this) {
+      case DropDownButtonPosition.bottomCenter:
+        return Alignment.topCenter;
+      case DropDownButtonPosition.bottomLeft:
+        return Alignment.topLeft;
+      case DropDownButtonPosition.bottomRight:
+        return Alignment.topRight;
+      case DropDownButtonPosition.topCenter:
+        return Alignment.bottomCenter;
+      case DropDownButtonPosition.topLeft:
+        return Alignment.bottomLeft;
+      case DropDownButtonPosition.topRight:
+        return Alignment.bottomRight;
+    }
+  }
+
+  /// Returns the alignment for the drop-down menu relative to the button.
+  Alignment get getMenuPosition {
+    switch (this) {
+      case DropDownButtonPosition.bottomCenter:
+        return Alignment.bottomCenter;
+      case DropDownButtonPosition.bottomLeft:
+        return Alignment.bottomLeft;
+      case DropDownButtonPosition.bottomRight:
+        return Alignment.bottomRight;
+      case DropDownButtonPosition.topCenter:
+        return Alignment.topCenter;
+      case DropDownButtonPosition.topLeft:
+        return Alignment.topLeft;
+      case DropDownButtonPosition.topRight:
+        return Alignment.topRight;
+    }
+  }
+}
--- test/basic_dropdown_button_test.dart
+++ test/basic_dropdown_button_test.dart
@@ -1,12 +1,110 @@
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:basic_dropdown_button/basic_dropdown_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:custom_drop_down_button/custom_drop_down_button.dart';
+void main() {
+  const buttonKey = Key('button_key');
+  const menuKey = Key('menu_key');
+  // Helper function to initialize the test environment
+  Future<void> initTest({
+    required WidgetTester tester,
+    required Widget child,
+  }) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: ListView(
+            children: [
+              const SizedBox(
+                height: 200,
+              ),
+              Center(child: child),
+              const SizedBox(
+                height: 200,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
 
-// void main() {
-//   test('adds one to input values', () {
-//     final calculator = Calculator();
-//     expect(calculator.addOne(2), 3);
-//     expect(calculator.addOne(-7), -6);
-//     expect(calculator.addOne(0), 1);
-//   });
-// }
+    await tester.pumpAndSettle();
+  }
+
+  testWidgets('Custom DropDown Button Default Position(bottom center)',
+      (tester) async {
+    final value = ValueNotifier<int?>(null);
+    await initTest(
+      tester: tester,
+      child: ValueListenableBuilder<int?>(
+        valueListenable: value,
+        builder: (BuildContext context, int? selected, child) {
+          return BasicDropDownButton(
+            key: buttonKey,
+            menuKey: menuKey,
+            buttonStyle: const ButtonStyle(),
+            buttonText: null,
+            customButton: (showHideMenu) =>
+                TextButton(onPressed: showHideMenu, child: const Text('test')),
+            menuItems: (hideMenu) => List.generate(
+              3,
+              (index) => TextButton(
+                key: Key('item_key_$index'),
+                child: Text('Item value is ${index + 1}'),
+                onPressed: () {
+                  hideMenu();
+                  value.value = index;
+                },
+              ),
+            ),
+            menuBorderRadius: BorderRadius.circular(
+              8,
+            ),
+            menuVerticalSpacing: 8,
+            menuBackgroundColor: Colors.grey,
+            showIndicatorIcon: true,
+            position: DropDownButtonPosition.topCenter,
+            buttonCloseMenuIcon: null,
+            buttonOpenMenuIcon: null,
+          );
+        },
+      ),
+    );
+
+    expect(find.byKey(buttonKey), findsOneWidget);
+
+    expect(find.byKey(menuKey), findsNothing);
+
+    expect(find.byKey(const Key('item_key_0')), findsNothing);
+
+    await tester.tap(find.byKey(buttonKey));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(buttonKey), findsOneWidget);
+
+    expect(find.byKey(menuKey), findsOneWidget);
+
+    expect(find.byKey(const Key('item_key_0')), findsOneWidget);
+
+    final menuPosition = tester.getCenter(find.byKey(menuKey));
+
+    final buttonPosition = tester.getCenter(find.byKey(buttonKey));
+
+    expect(menuPosition.dx, buttonPosition.dx);
+
+    expect(menuPosition.dy < buttonPosition.dy, isTrue);
+
+    await tester.tap(find.byKey(buttonKey));
+
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(buttonKey), findsOneWidget);
+
+    expect(find.byKey(menuKey), findsNothing);
+
+    expect(find.byKey(const Key('item_key_0')), findsNothing);
+
+    expect(value.value, null);
+  });
+}
